a5d032417007c3f76c505707aaffb22dfaf8bc1f
Ershov A.S. <sashaershov212@gmail.com>
2026-03-11 21:41:15 +0300

    set_velocities function was added (Makswell)

--- system.h
+++ system.h
@@ -1,6 +1,7 @@
 #include <vector>
 #include <cmath>
 #include <iostream>
+#include <random>
 
 using namespace std;
 
@@ -57,13 +58,13 @@ class system {
 		int N;				//число частиц
 		int sizeX, sizeY, sizeZ;	//размеры решетки
 		point3D*** lattice;		//инициализация кристаллической решетки
-
+		double avg_mass;
 	public:
-		system() : N(0), sizeX(0), sizeY(0), sizeZ(0) {
+		system() : N(0), sizeX(0), sizeY(0), sizeZ(0), avg_mass(1.0) {
 			lattice = nullptr;
 		}
 
-		system(int n, double distance) {
+		system(int n, double distance, double mass = 1.0) : avg_mass(mass) {
 			if (n >= 0) {
 				sizeX = sizeY = sizeZ = static_cast<int>(ceil(cbrt(n)));	//достраеваем до куба (N >= n)
 				N = sizeX * sizeY * sizeZ;
@@ -111,5 +112,51 @@ class system {
 			}
 		}	
 
+		void set_velocities(double t_target = 298.0, double k_boltzmann = 1.0) {
+    			random_device rd; 
+    			mt19937 gen(rd()); // генератор Мерсенна Твистера
+    			uniform_real_distribution<double> dist(0.0, 1.0);
+
+			double sigma = sqrt(k_boltzmann * t_target / avg_mass);
+			
+			//vector<double> all_vx, all_vy, all_vz;
+
+			//all_vx.reserve(N);
+			//all_vy.reserve(N);
+			//all_vz.reserve(N);
+			
+			// метод Бокса-Мюллера
+			for (int i = 0; i < sizeX; ++i) {
+				for (int j = 0; j < sizeY; ++j) {
+			    		for (int k = 0; k < sizeZ; ++k) {
+						
+						// x
+						double u1 = dist(gen);
+						double u2 = dist(gen);
+						u1 = max(u1, numeric_limits<double>::min());
+						double vx = sigma * sqrt(-2.0 * log(u1)) * cos(2.0 * M_PI * u2);
+				
+						// y
+						u1 = dist(gen);
+						u2 = dist(gen);
+						u1 = max(u1, numeric_limits<double>::min());
+						double vy = sigma * sqrt(-2.0 * log(u1)) * cos(2.0 * M_PI * u2);
+
+						// z
+						u1 = dist(gen);
+						u2 = dist(gen);
+						u1 = max(u1, numeric_limits<double>::min());
+						double vz = sigma * sqrt(-2.0 * log(u1)) * cos(2.0 * M_PI * u2);
+				
+						lattice[i][j][k].set_vel(vx, vy, vz);
+				
+						//all_vx.push_back(vx);
+						//all_vy.push_back(vy);
+						//all_vz.push_back(vz);
+			    		}
+				}
+			}
+		}	
+				
 };
 
